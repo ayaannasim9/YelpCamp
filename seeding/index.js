23c7371bf99516d0378a02eebcd5884edfd911c5
ayaannasim9 <ayaannasim9@gmail.com>
2025-08-27 20:38:57 +0530

    Added in the new keys

--- seeding/index.js
+++ seeding/index.js
@@ -16,9 +16,13 @@ async function seedDB(){
     await Campground.deleteMany({});
     for(let i=0; i<50; i++){
         let randomNumber=Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*20)+10;
         const camp=new Campground({
             location:`${cities[randomNumber].city}, ${cities[randomNumber].state}`,
             title:`${sample(descriptors)} ${sample(places)}`,
+            image:`https://picsum.photos/400?random=${Math.random()}`,
+            description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam sit illo eos expedita. Dolorem rerum dignissimos consequatur fugit omnis doloremque officia cupiditate dolorum possimus. Nihil explicabo deleniti ea suscipit assumenda.',
+            price:price
         })
         await camp.save();
     }
